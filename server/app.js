20cf641a8e85ee4d09c2e3fba41e950f6a489910
x22231757 <126015792+x22231757@users.noreply.github.com>
2024-04-14 10:48:54 +0100

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,1 +1,14 @@
+var grpc = require("@grpc/grpc-js")
+var protoLoader = require("@grpc/proto-loader")
+var PROTO_PATH = __dirname + "/protos/proto.proto"
+var packageDefinition = protoLoader.loadSync(
+  PROTO_PATH
+)
+var proto = grpc.loadPackageDefinition(packageDefinition).package_name
 
+
+
+var server = new grpc.Server()
+server.bindAsync("0.0.0.0:40000", grpc.ServerCredentials.createInsecure(), function() {
+  server.start()
+})
