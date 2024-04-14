41f120bdac790e89d18d0d67f3f3953b97376f18
x22231757 <126015792+x22231757@users.noreply.github.com>
2024-04-14 14:32:16 +0100

    Update app.js
    Comit after working checkAttendance function

--- server/app.js
+++ server/app.js
@@ -1,25 +1,15 @@
-//loading service definition
 var grpc = require("@grpc/grpc-js")
 var protoLoader = require("@grpc/proto-loader")
-
-
 var PROTO_PATH = __dirname + "/protos/attendance.proto"
-//load proto file
 var packageDefinition = protoLoader.loadSync(PROTO_PATH)
 var attendance_proto = grpc.loadPackageDefinition(packageDefinition).attendance
 
-//service implementation where the logic resides
 var studentList = [
-  {studentName: "John, Smith",
-    attendence: 0},
-  {studentName: "Sally, Robinson",
-    attendence: 0},
-  {studentName: "Mary, Reilley",
-    attendance:0},
-  {studentName: "Vera, Smith",
-    attendance:0},
-  {studentName: "Linda, Gilmore",
-    attendance:0}
+  {studentName: "John, Smith", attendence: 0},
+  {studentName: "Sally, Robinson", attendence: 0},
+  {studentName: "Mary, Reilley", attendance:0},
+  {studentName: "Vera, Smith", attendance:0},
+  {studentName: "Linda, Gilmore", attendance:0}
 ]
 
 var attendenceCode = "1T70A"
@@ -28,18 +18,12 @@ function checkAttendance(call, callback){
   try{
     var studentNameRequest = call.request.studentNameRequest
     var codeRequest = call.request.codeRequest
-    //const ifInList = studentNameRequest.every(studentName => studentList.find(item => item.studentName === studentName))
-    let objIndex = studentList.findIndex(
+    var objIndex = studentList.findIndex(
       (temp) => temp["studentName"] === studentNameRequest
       );
     if(objIndex !=-1){
       if (codeRequest == attendenceCode) {
             studentList[objIndex].attendance = 1
-            //Object.keys(studentList).forEach((item) => {
-            //  if(studentList[item].studentName == studentNameRequest) {
-            //      studentList[item].attendance = 1
-            //  }
-            //})
             callback(null, {
               message : "Success, your attendence has been recorded.",
               result: studentList[objIndex].attendance
@@ -47,7 +31,8 @@ function checkAttendance(call, callback){
 
       } else {
         callback(null, {
-        message : "The attendence code is wrong please enter the correct code."
+          message : "The attendence code is wrong please enter the correct code.",
+          result: studentList[objIndex].attendance
       })
       }
     }else{
@@ -61,8 +46,6 @@ function checkAttendance(call, callback){
   })}
 }
 
-
-//make a new server
 var server = new grpc.Server()
 server.addService(attendance_proto.AttendanceService.service, {checkAttendance:checkAttendance})
 
