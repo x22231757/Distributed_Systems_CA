ce484e231d6b56b2008c1f83b9fadc269f2c0d85
x22231757 <126015792+x22231757@users.noreply.github.com>
2024-04-14 14:29:52 +0100

    Update app.js
    Working Unary checkAttendence method

--- client/app.js
+++ client/app.js
@@ -1,9 +1,26 @@
-var readlineSync = require('readline-sync')
+var readlineSync = require("readline-sync")
 var grpc = require("@grpc/grpc-js")
 var protoLoader = require("@grpc/proto-loader")
-var PROTO_PATH = __dirname + "/protos/proto.proto"
-
+var PROTO_PATH = __dirname + "/protos/attendance.proto"
 var packageDefinition = protoLoader.loadSync(PROTO_PATH)
-var proto = grpc.loadPackageDefinition(packageDefinition).package_name
-var client = new proto.CalcService("0.0.0.0:40000", grpc.credentials.createInsecure());
+var attendance_proto = grpc.loadPackageDefinition(packageDefinition).attendance
+var client = new attendance_proto.AttendanceService("0.0.0.0:40000", grpc.credentials.createInsecure());
 
+var studentNameRequest = readlineSync.question("What is your full name? (Name, Surname)")
+var codeRequest  = readlineSync.question("What is the attendence code made available by the teacher?")
+
+try{
+  client.checkAttendance({
+    studentNameRequest: studentNameRequest,
+    codeRequest: codeRequest},
+    function(error, response){
+      try{
+        console.log(response.message)
+        console.log("Your attendence is now marked as : " + response.result)
+      }catch(e){
+        console.log("Could not connect to server.")
+      }
+    })
+}catch(e){
+  console.log("An error Occured.")
+}
