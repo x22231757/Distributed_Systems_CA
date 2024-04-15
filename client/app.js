dc74b160dbe30157547df971b24450d3705b3484
x22231757 <126015792+x22231757@users.noreply.github.com>
2024-04-15 22:45:31 +0100

    Update app.js
    Chat implmentation

--- client/app.js
+++ client/app.js
@@ -1,12 +1,14 @@
 var readlineSync = require("readline-sync")
 var grpc = require("@grpc/grpc-js")
 var protoLoader = require("@grpc/proto-loader")
+var readline = require('readline')
 
 //specify which service would you like to use:
 var service = readlineSync.question (
   "Which service would you like to use? \n"
 + " \t 1 for Atendance Service \n"
-+ " \t 2 for Educational Portal Service \n" )
++ " \t 2 for Educational Portal Service \n"
++ " \t 3 for Q&A Chat Service \n" )
 
 service = parseInt(service)
 
@@ -77,7 +79,6 @@ if(service === 1 ) { // Attendance service start
   + " \t 1 to complete a quiz \n"
   + " \t 2 to upload a quiz \n")
 
-
   education_portal_action = parseInt(education_portal_action)
   if(education_portal_action === 1 ) {
 
@@ -133,4 +134,51 @@ if(service === 1 ) { // Attendance service start
    }
    call .end()
   }
-}//Educational portal service ends
+} else if(service ===3){//Educational portal service ends; Chat service starts
+    var PROTO_PATH = __dirname + "/protos/chat.proto"
+    var packageDefinition = protoLoader.loadSync(PROTO_PATH)
+    var chat_proto = grpc.loadPackageDefinition(packageDefinition).chat
+    var client = new chat_proto.ChatService ("0.0.0.0:40000", grpc.credentials.createInsecure());
+
+    var name = readlineSync.question("What is your name? ")
+    var call = client.sendMessage();
+
+    call.on('data', function(resp){
+       console.log(resp .name + ': ' + resp .message )
+    }) ;
+
+    call.on('end', function(){
+     })
+
+    call.on('error', function(e){
+       console.log("Cannot connect to chat server")
+     })
+
+    call.write({
+      message : name + " joined the chat .",
+      name : name
+    })
+
+    var rl = readline.createInterface({
+       input :process.stdin,
+       output : process.stdout
+    })
+
+    rl.on("line", function(message){
+      if(message.toLowerCase() === 'quit'){
+        call.write({
+          message: name + " left the chatroom.",
+          name:name
+        });
+        call.end();
+        rl.close();
+      }else{
+        call.write({
+          message: message,
+          name:name
+        })
+      }
+    })
+
+
+}//Chat service ends;
